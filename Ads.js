c1cb622c7474693f157990fd971ad8be23cfc02e
Brandon <mercadobrandon33@yahoo.com>
2018-06-17 19:25:57 -0700

    Added Carousel for ads

--- Ads.js
+++ Ads.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
-import { } from 'react-native';
+import { View, Image} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
-class Ads extends Component {
+images = [{picture: require('./colonel.png')}, {picture: require('./kfcad1.jpg')}, {picture: require('./kfcad2.jpg')}];
 
+export default class Ads extends Component {
+    _renderItem ({item, index}) {
+        return (
+            <View>
+                <Image style={{marginTop: 25, marginBottom: 25, height: 625, width: 625}} source={item.picture}/>
+            </View>
+        );
+    }
+
+    render() {
+        return (
+        <Carousel
+            data={images}
+            itemWidth={625}
+            itemHeight={625}
+            sliderHeight={625}
+            sliderWidth={625}
+            renderItem={this._renderItem}
+            lockScrollWhileSnapping={true}
+            autoplay={true}
+            loop={true}
+        />
+        );
+    }
 }
--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import PhoneNumber from './PhoneNumber';
 import StampsDetails from './StampsDetail';
+import Ads from './Ads';
 import { AppRegistry, Text, View, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class template extends Component {
+
   render() {
     return (
     <View style={{flex: 500, flexDirection: 'row', backgroundColor: 'white'}}>
-      <View style={{flex: 300}}></View>
+      <View style={{flex: 300}}><Ads /></View>
       <View style={{flex: 1,backgroundColor: 'dimgray'}}></View>
       <View style={{flex: 190}}><PhoneNumber navigation={this.props.navigation}/></View>
     </View>
